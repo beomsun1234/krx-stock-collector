37accd3add54b1812fc71f6db51b9d71b7e30011
beomsun1234 <beomsun22@gmail.com>
2023-10-04 18:42:04 +0900

    test: test 케이스 추가

--- krx/krx_test.go
+++ krx/krx_test.go
@@ -1,11 +1,13 @@
 package krx_test
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/beomsun1234/krx-stock-collector/krx"
+	"github.com/stretchr/testify/assert"
 )
 
 type MockHTTPClient struct {
@@ -13,8 +15,8 @@ type MockHTTPClient struct {
 
 func (c *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	csvData := `Column1,Column2,Column3,Column4,Column5,Column6,Column7,Column8,Column9
-2023/06/08,Value1-2,Value1-3,Value1-4,Value1-5,Value1-6,Value1-7,Value1-8,Value1-9
-2023/06/07,Value2-2,Value2-3,Value2-4,Value2-5,Value2-6,Value1-7,Value1-8,Value1-9
+Value1-1,Value1-2,Value1-3,Value1-4,Value1-5,Value1-6,Value1-7,Value1-8,Value1-9
+Value2-1,Value2-2,Value2-3,Value2-4,Value2-5,Value2-6,Value1-7,Value1-8,Value1-9
 `
 	res := httptest.NewRecorder()
 	res.Header().Set("Content-Type", "text/csv")
@@ -22,11 +24,55 @@ func (c *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return res.Result(), nil
 }
 
+type MockHTTPClient2 struct {
+}
+
+func (c *MockHTTPClient2) Do(req *http.Request) (*http.Response, error) {
+	csvData := `Column1,Column2,Column3,Column4,Column5,Column6,Column7,Column8,Column9
+2023/09/02,Value1-2,Value1-3,Value1-4,Value1-5,Value1-6,Value1-7,Value1-8,Value1-9
+2023/09/01,Value2-2,Value2-3,Value2-4,Value2-5,Value2-6,Value1-7,Value1-8,Value1-9
+`
+	res := httptest.NewRecorder()
+	res.Header().Set("Content-Type", "text/csv")
+	res.WriteString(csvData)
+	return res.Result(), nil
+}
+
+type MockErrorClient struct {
+}
+
+func (c *MockErrorClient) Do(req *http.Request) (*http.Response, error) {
+	return nil, errors.New("error")
+}
+
 func Test_GetStockInfo(t *testing.T) {
 	t.Run("GetStcokPrice test", func(t *testing.T) {
-		krx := krx.New{
-			Client: &MockHTTPClient{},
-		}
-		krx.GetStockInfo()
+		krx := krx.New(&MockHTTPClient{})
+
+		result := krx.GetStockInfo()
+
+		assert.Equal(t, 2, len(result))
+	})
+}
+
+func Test_GetStockInfo2(t *testing.T) {
+	t.Run("failed to get BusinessDay", func(t *testing.T) {
+		krx := krx.New(&MockErrorClient{})
+
+		result := krx.GetStockInfo()
+
+		assert.Equal(t, 0, len(result))
+		assert.Nil(t, result)
+	})
+}
+
+func Test_GetBusinessDay(t *testing.T) {
+	t.Run("GetBusinessDay test", func(t *testing.T) {
+		krx := krx.New(&MockHTTPClient2{})
+
+		day, err := krx.GetBusinessDay()
+
+		assert.Equal(t, "20230902", day)
+		assert.Nil(t, err)
 	})
 }
