65098a25015d434ef3ae2ec0f0dd393f84c54e53
beomsun1234 <beomsun22@gmail.com>
2023-10-05 19:50:29 +0900

    test: csv 사이즈 체크 test

--- krx/krx_test.go
+++ krx/krx_test.go
@@ -14,9 +14,9 @@ type MockHTTPClient struct {
 }
 
 func (c *MockHTTPClient) Do(req *http.Request) (*http.Response, error) {
-	csvData := `Column1,Column2,Column3,Column4,Column5,Column6,Column7,Column8,Column9
-Value1-1,Value1-2,Value1-3,Value1-4,Value1-5,Value1-6,Value1-7,Value1-8,Value1-9
-Value2-1,Value2-2,Value2-3,Value2-4,Value2-5,Value2-6,Value1-7,Value1-8,Value1-9
+	csvData := `Column1,Column2,Column3,Column4,Column5,Column6,Column7,Column8,Column9,Column10,Column11,Column12,Column13
+Value1-1,Value1-2,Value1-3,Value1-4,Value1-5,Value1-6,Value1-7,Value1-8,Value1-9,Value1-10,Value1-11,Value1-12,Value1-13
+Value2-1,Value2-2,Value2-3,Value2-4,Value2-5,Value2-6,Value2-7,Value2-8,Value2-9,Value2-10,Value2-11,Value2-12,Value2-13
 `
 	res := httptest.NewRecorder()
 	res.Header().Set("Content-Type", "text/csv")
@@ -58,6 +58,17 @@ func Test_GetStockInfo(t *testing.T) {
 func Test_GetStockInfo2(t *testing.T) {
 	t.Run("failed to get BusinessDay", func(t *testing.T) {
 		krx := krx.New(&MockErrorClient{})
+
+		result := krx.GetStockInfo()
+
+		assert.Equal(t, 0, len(result))
+		assert.Nil(t, result)
+	})
+}
+
+func Test_GetStockInfo3(t *testing.T) {
+	t.Run("csv Column < 12 -> nil ", func(t *testing.T) {
+		krx := krx.New(&MockHTTPClient2{})
 
 		result := krx.GetStockInfo()
 
